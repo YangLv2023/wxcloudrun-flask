a56a6329d8d2c6eb253f7c642c44009547ce742c
吕阳 <159-ly@users.noreply.5ca75618cd54>
2023-05-11 17:37:08 +0800

    ai回复加入

---
+++ aiSay.py
@@ -0,0 +1,37 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+# @Project : gpt4free
+# @Author : pei
+# @Time : 2023-05-09 17:13
+# @Describe : 
+"""
+from gptAi.gpt4free import you
+from flask import Flask, jsonify, request, redirect, render_template
+import gptAi.gpt4free
+from gptAi.gpt4free import Provider, quora, forefront
+from gptAi.gpt4free import usesless
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+app.debug = True
+chat = {}
+chat_gpt = []
+num = 1
+message_id = ""
+
+
+@app.route('/gpt', methods=['POST'])
+def gpt():
+    global num, chat, message_id
+    if request.method == 'POST':
+        prompt = request.form.get('content')
+        message_id = request.form.get('message_id')
+        response = usesless.Completion.create(prompt=prompt, parentMessageId=message_id)
+        message_id = response["id"]
+        print(response['text'])
+        return jsonify({"response": response['text'], "message_id": message_id})
+
+
+if __name__ == '__main__':
+    app.run()
